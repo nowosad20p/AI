bc245283a5c501d4dac4bef8c4c4bae447e29264
nowosad20p <91263440+nowosad20p@users.noreply.github.com>
2023-11-07 21:42:04 +0100

    Update main.js

--- main.js
+++ main.js
@@ -32,6 +32,16 @@ const traffic=[
     new Car(road.getLaneCenter(1),-500,30,50,"DUMMY",2),
     new Car(road.getLaneCenter(1),-700,30,50,"DUMMY",2),
     new Car(road.getLaneCenter(2),-700,30,50,"DUMMY",2),
+    new Car(road.getLaneCenter(1),-900,30,50,"DUMMY",2),
+    new Car(road.getLaneCenter(1),-1100,30,50,"DUMMY",2),
+    new Car(road.getLaneCenter(2),-1100,30,50,"DUMMY",2),
+    new Car(road.getLaneCenter(0),-1300,30,50,"DUMMY",2),
+    new Car(road.getLaneCenter(1),-1300,30,50,"DUMMY",2),
+    new Car(road.getLaneCenter(2),-1500,30,50,"DUMMY",2),
+    new Car(road.getLaneCenter(0),-1700,30,50,"DUMMY",2),
+    new Car(road.getLaneCenter(1),-1700,30,50,"DUMMY",2),
+    new Car(road.getLaneCenter(2),-1900,30,50,"DUMMY",2),
+
 ];
 
 animate();
